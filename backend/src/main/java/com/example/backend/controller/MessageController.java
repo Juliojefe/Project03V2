abc16ca7f773ab574aa827ae20bfd7008c2b0fa0
Juliojefe <j.c.jf285@gmail.com>
2025-05-06 23:45:13 +0100

    message crud (text only messages, text and image messages)

--- backend/src/main/java/com/example/backend/controller/MessageController.java
+++ backend/src/main/java/com/example/backend/controller/MessageController.java
@@ -1,12 +1,10 @@
 package com.example.backend.controller;
 
+import com.example.backend.dto.MessageDTO;
 import com.example.backend.model.Message;
 import com.example.backend.services.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.example.backend.repository.MessageRepository;
 
 import java.util.List;
@@ -19,8 +17,29 @@ public class MessageController {
     @Autowired
     private MessageService messageService;
 
-//    @GetMapping("/chat/{chatId}/messages")
-//    public List<Message> getMessages(@PathVariable int chatId) {
-//        return messageRepository.getMessagesByChatId(chatId);
-//    }
+    @GetMapping("/chat/{chatId}/")
+    public List<MessageDTO> getChatMessages(@PathVariable int chatId) {
+        return messageService.getChatMessages(chatId);
+    }
+
+    @PostMapping()
+    public int sendMessage(@RequestParam String content, @RequestParam int userId, @RequestParam int chatId) {
+        return messageService.sendNewMessage(content, userId, chatId);
+    }
+
+    @PatchMapping("/{messageId}")
+    public boolean editMessage(@PathVariable int messageId, @RequestParam String newContent) {
+        return messageService.editMessage(messageId, newContent);
+    }
+
+    @DeleteMapping("/{messageId}")
+    public boolean deleteMessage(@PathVariable int messageId) {
+        return messageService.deleteMessage(messageId);
+    }
+
+    @PostMapping("/with-image")
+    public int sendMessageWithImage(@RequestParam String content, @RequestParam int userId, @RequestParam int chatId, @RequestParam String imageUrl) {
+        return messageService.sendNewMessageWithImage(content, userId, chatId, imageUrl);
+    }
+
 }
--- backend/src/main/java/com/example/backend/repository/ImageRepository.java
+++ backend/src/main/java/com/example/backend/repository/ImageRepository.java
@@ -1,9 +1,14 @@
 package com.example.backend.repository;
 
+import com.example.backend.enums.RelatedType;
 import com.example.backend.model.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 
 public interface ImageRepository extends JpaRepository<Image, Integer> {
+    List<Image> findByRelatedIdAndRelatedType(int relatedId, RelatedType relatedType);
+    void deleteByRelatedIdAndRelatedType(int relatedId, RelatedType relatedType);
 }
