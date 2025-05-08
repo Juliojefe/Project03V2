d9ffa8ab8504cb5d812bad2fe86bca8ae120f12a
Juliojefe <j.c.jf285@gmail.com>
2025-05-08 20:39:37 +0100

    fixed routing for web

--- frontend/web/app/_layout.tsx
+++ frontend/web/app/_layout.tsx
@@ -1,3 +1,16 @@
+// import { Stack } from "expo-router";
+// import Navbar from "../components/Navbar";
+
+// export default function RootLayout() {
+//   return (
+//     <Stack
+//       screenOptions={{
+//         header: () => <Navbar />,
+//       }}
+//     />
+//   );
+// }
+
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
